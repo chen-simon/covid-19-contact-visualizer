cec08825568debd3b6a744d19066c75185243a3a
narwhalblast <simonsc.chen@mail.utoronto.ca>
2021-04-10 14:02:45 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,7 +1,59 @@
-import networkx
+import networkx as nx
+from plotly.graph_objs import Scatter, Figure
 from graph import Graph
 
 
 # Degrees visualization
-def render_degrees_apart(graph: Graph, ) -> None:
-    pass
+def render_degrees_apart(graph: Graph, init_infected: set[str]) -> None:
+    """ Render the degrees visualization given a graph, and the initial set of infected people.
+
+    Preconditions:
+        - all(not person.infected for person in graph._people.values())
+    """
+    graph.set_infected(init_infected)
+    graph.recalculate_degrees()
+
+    graph_nx = graph.to_networkx(max_vertices)
+
+    pos = getattr(nx, layout)(graph_nx)
+
+    x_values = [pos[k][0] for k in graph_nx.nodes]
+    y_values = [pos[k][1] for k in graph_nx.nodes]
+    labels = list(graph_nx.nodes)
+    kinds = [graph_nx.nodes[k]['kind'] for k in graph_nx.nodes]
+
+    colours = [BOOK_COLOUR if kind == 'book' else USER_COLOUR for kind in kinds]
+
+    x_edges = []
+    y_edges = []
+    for edge in graph_nx.edges:
+        x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
+        y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+    trace3 = Scatter(x=x_edges,
+                     y=y_edges,
+                     mode='lines',
+                     name='edges',
+                     line=dict(color=LINE_COLOUR, width=1),
+                     hoverinfo='none',
+                     )
+    trace4 = Scatter(x=x_values,
+                     y=y_values,
+                     mode='markers',
+                     name='nodes',
+                     marker=dict(symbol='circle-dot',
+                                 size=5,
+                                 color=colours,
+                                 line=dict(color=VERTEX_BORDER_COLOUR, width=0.5)
+                                 ),
+                     text=labels,
+                     hovertemplate='%{text}',
+                     hoverlabel={'namelength': 0}
+                     )
+
+    data1 = [trace3, trace4]
+    fig = Figure(data=data1)
+    fig.update_layout({'showlegend': False})
+    fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
+    fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
+
