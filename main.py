40f0b634d02360dd5fb89799a9af569f2f8d9cfe
narwhalblast <simonsc.chen@mail.utoronto.ca>
2021-04-11 14:46:34 -0400

    Added runner function for Degrees Risk Visualization

--- main.py
+++ main.py
@@ -3,4 +3,14 @@
 """
 from networkx import nx
 from plotly.graph_objs import Scatter, Figure
+import visualization
 import data_processing
+
+
+def run_degrees_example() -> None:
+    """ Run the example degrees risk visualization using the sample graph of people.
+    """
+    graph = data_processing.load_graph_csv('data/persons.csv', 'data/connections.csv')
+    init_infected = {'WJ5751'}
+
+    visualization.render_degrees_apart(graph, init_infected)
