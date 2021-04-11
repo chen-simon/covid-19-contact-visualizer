d812c6e1d286eb532445475c13b2f6d2049dd4e2
Salman Husainie <75236475+Just-Call-Sal@users.noreply.github.com>
2021-04-11 15:00:38 -0400

    Added docstring to main.py
    3 pm

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-""" COVID-19 Contact Visualizer
-    Copyright Simon Chen, Patricia Ding, Salman Husainie, Makayla Duffus
+"""CSC111 Project: COVID-19 Contact Visualizer
+
+Module Description
+==================
+Main Module
+This module runs the entire COVID-19 Contact Visualizer. # TODO: Make this better after done.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2021 Simon Chen, Patricia Ding, Salman Husainie, Makayla Duffus
 """
 from networkx import nx
 from plotly.graph_objs import Scatter, Figure
